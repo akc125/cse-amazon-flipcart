ccef052d86f3dc3756a8226986f2facfdab5f76e
kareem <akckl25@gmail.com>
2022-06-11 10:19:15 +0530

    cse amazon flipcar

---
+++ javas.js
@@ -0,0 +1,56 @@
+
+
+
+let dt=document.createElement("button");
+dt.innerText="delete";
+dt.classList.add("dele")
+ros.appendChild(dt);
+
+let ed=document.createElement("button");
+ed.innerText="edit";
+ed.classList.add("edi")
+ros.appendChild(ed);
+
+
+
+
+
+
+let dt=document.createElement("button");
+dt.innerText="delete";
+dt.classList.add("dele")
+ros.appendChild(dt);
+
+
+
+
+fox.value="";
+fox.focus();
+
+
+}
+
+function dol(event){
+if(event.target.classList[0]="dele"){
+let item=event.target.parentElement;
+item.remove();
+
+}
+function edol(event){
+if(event.target.classList[0]="edi"){
+    let.toy=prompt("edit");
+let item=event.target.parentElement;
+item.innerText=toy;
+
+}
+
+
+
+
+
+
+bot.addEventListener("click", volk)
+b.addEventListener("click", dol)
+b.addEventListener("click", edol)
+
+     
